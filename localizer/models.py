3d67fcabc8fe897cc55a35c54e26271e0225775e
Benjamin Wild <b.w@fu-berlin.de>
2015-12-17 14:08:57 +0100

    adapt models to continuous output values

--- localizer/models.py
+++ localizer/models.py
@@ -39,41 +39,29 @@ def get_filter_network():
 
     model.add(Flatten())
 
-    model.add(Dense((8 * 8) * 64, 1024, init='glorot_uniform'))
+    model.add(Dense((8 * 8) * 64, 1024))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
-    model.add(Dense(1024, 1024, init='glorot_uniform'))
+    model.add(Dense(1024, 1024))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
-    model.add(Dense(1024, 2))
-    model.add(Activation('softmax'))
+    model.add(Dense(1024, 1))
+    model.add(Activation('sigmoid'))
 
-    model.compile(loss='categorical_crossentropy', optimizer="adam")
+    model.compile(loss='mse', optimizer="adam")
 
     return model
-
-def saliency_error(y_true, y_pred):
-    y_pred = T.clip(y_pred, epsilon, 1.0 - epsilon)
-    base_loss = T.nnet.binary_crossentropy(y_pred, y_true)
-    loss = T.set_subtensor(base_loss[T.eq(y_true, 1)],
-                           base_loss[T.eq(y_true, 1)], inplace=False)
-    return loss.mean(axis=-1)
-
 
 def get_saliency_network(train=True):
     model = Sequential()
 
-    model.add(Convolution2D(4, 1, 3, 3, activation='relu'))
+    model.add(Convolution2D(32, 1, 3, 3, activation='relu', border_mode='same'))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(8, 4, 3, 3, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(16, 8, 3, 3, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Convolution2D(32, 16, 10, 10, activation='tanh'))
+    model.add(Convolution2D(128, 32, 16, 16, activation='tanh'))
     model.add(Dropout(0.5))
-    model.add(Convolution2D(1, 32, 1, 1, activation='sigmoid'))
+    model.add(Convolution2D(1, 128, 1, 1, activation='sigmoid'))
 
     if train:
         model.add(Flatten())

986cc2e8beeb2ec2a82149f4f505ec5fb9ad4ea9
Benjamin Wild <b.w@fu-berlin.de>
2015-12-17 14:03:32 +0100

    add filtersize to config

--- localizer/config.py
+++ localizer/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 data_imsize = (64, 64)
+filtersize = (16, 16)
 
 filenames_mmapped = {name: '{}.mmapped'.format(name) for name in
                      ['xtrain', 'ytrain', 'xval', 'yval', 'xtest', 'ytest']}

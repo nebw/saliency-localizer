57a24e64e8e4ca61f0381d7ada87d8844240e5d2
berleon <github@leon-sixt.de>
2015-12-17 17:06:33 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='saliceny-localizer',
+      version='0.0.1',
+      description='',
+      install_requires=[
+            "numpy>=1.9",
+            "keras",
+            "scikit-image>=0.11.3",
+            "theano",
+            "seaborn",
+            "h5py",
+      ],
+      packages=['localizer']
+)

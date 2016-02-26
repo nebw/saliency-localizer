6a0b993e8f5e20a1ec9a694638666a8d58a58a6c
Benjamin Wild <nimajnebwild@gmail.com>
2016-02-26 15:12:43 +0000

    update class API

--- localizer/localizer.py
+++ localizer/localizer.py
@@ -2,6 +2,9 @@
 
 from os.path import isfile, join, isdir
 from localizer import models, util, keras_helpers, config
+from skimage.filters import gaussian_filter
+
+import numpy as np
 
 
 class Localizer:
@@ -19,7 +22,6 @@ class Localizer:
 
         self.saliency_network = models.get_saliency_network(
             train=True, compile=False)
-        self.filter_network = models.get_filter_network(compile=False)
 
     def load_weights(self, data_dir):
         if not isdir(data_dir):
@@ -28,29 +30,22 @@ class Localizer:
         if isfile(saliency_weight_file):
             self.logger.info('Restoring saliency network weights...')
             self.saliency_network.load_weights(saliency_weight_file)
-
-        filter_weight_file = join(data_dir, 'filter_weights')
-        if isfile(filter_weight_file):
-            self.logger.info('Restoring filter network weights...')
-            self.filter_network.load_weights(filter_weight_file)
+        else:
+            raise ValueError('invalid weight file')
 
     def train_saliency(self):
         self.logger.info('Training saliency network...')
         raise NotImplemented()
 
-    def train_filter(self):
-        self.logger.info('Training filter network...')
-        # TODO: load or reevaluate thresholds
-        raise NotImplemented()
-
-    def compile(self):
+    def compile(self, image_shape=(3060, 4060)):
         self.logger.info('Compiling saliency network convolution function...')
-        saliency_conv_model = models.get_saliency_network(train=False)
+        ratio = config.filtersize[0] / config.data_imsize[0]
+        targetsize = np.round(np.array(image_shape) * ratio).astype(int)
+        saliency_conv_model = models.get_saliency_network(train=False, shape=targetsize)
         self.convolution_function = keras_helpers.get_convolution_function(
             self.saliency_network, saliency_conv_model)
 
-    def detect_tags(self, image_path, saliency_threshold):
-        # TODO: default value for saliency threshold based on evaluation
+    def detect_tags(self, image_path, saliency_threshold=0.5):
         image, image_filtersize, targetsize = util.preprocess_image(
             image_path, config.filtersize)
 
@@ -58,9 +53,9 @@ class Localizer:
             image_filtersize.reshape((1, 1, image_filtersize.shape[0],
                                       image_filtersize.shape[1])))
 
+        saliency[0][0, 0] = gaussian_filter(saliency[0][0, 0], sigma=2.)
+
         candidates = util.get_candidates(saliency, saliency_threshold)
         rois, saliencies = util.extract_rois(candidates, saliency, image)
-
-        # TODO: filter candidates using filter network
 
         return saliencies, candidates, rois

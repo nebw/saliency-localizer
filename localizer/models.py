6c2f6360941ee1aa0d6c70c545877b413c5965bc
Benjamin Wild <nimajnebwild@gmail.com>
2016-02-26 14:34:57 +0000

    new model definitions for saliency and filter network

--- localizer/models.py
+++ localizer/models.py
@@ -2,40 +2,42 @@
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
 from keras.objectives import mse
-from localizer.config import data_imsize
+from localizer.config import data_imsize, filtersize
+from keras.optimizers import SGD
 
 
 def get_filter_network():
     model = Sequential()
 
-    model.add(Convolution2D(16, 2, 2,
+    model.add(Convolution2D(8, 2, 2,
                             input_shape=(1, data_imsize[0], data_imsize[1]),
                             border_mode='same'))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(8, 2, 2))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D())
+    model.add(Dropout(0.2))
+
     model.add(Convolution2D(16, 2, 2, border_mode='same'))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(Convolution2D(16, 2, 2))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D())
     model.add(Dropout(0.2))
 
     model.add(Convolution2D(32, 2, 2, border_mode='same'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(32, 2, 2, border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(Convolution2D(32, 2, 2))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D())
-    model.add(Dropout(0.2))
-
-    model.add(Convolution2D(64, 2, 2, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 2, 2, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 2, 2))
-    model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D())
     model.add(Dropout(0.2))
 
@@ -43,32 +45,42 @@ def get_filter_network():
 
     model.add(Dense(1024))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-
-    model.add(Dense(1024))
-    model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
     model.add(Dense(1))
-    model.add(Activation('sigmoid'))
+    model.add(Activation('softmax'))
+    model.add(BatchNormalization())
 
-    model.compile(loss='mse', optimizer="adam")
+    model.compile(loss='categorial_crossentropy', optimizer="adam")
 
     return model
 
 
-def get_saliency_network(train=True):
+def get_saliency_network(train=True, learning_rate=0.01, shape=None):
+    if train:
+        input_shape = (1, filtersize[0], filtersize[1])
+    else:
+        input_shape = (1, shape[0], shape[1])
+
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(1, None, None),
-                            activation='relu', border_mode='same'))
+    model.add(Convolution2D(32, 5, 5, activation='relu', border_mode='valid', subsample=(2, 2), input_shape=input_shape))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(128, 16, 16, activation='tanh'))
+
+    model.add(Convolution2D(64, 5, 5, activation='relu', border_mode='valid', subsample=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Convolution2D(128, 5, 5, activation='relu', border_mode='valid'))
     model.add(Dropout(0.5))
+
     model.add(Convolution2D(1, 1, 1, activation='sigmoid'))
 
     if train:
         model.add(Flatten())
+    else:
+        model.add(UpSampling2D())
+        model.add(UpSampling2D())
 
-    model.compile('adam', mse)
+    model.compile(SGD(lr=learning_rate, momentum=0.9, nesterov=True), mse)
 
     return model
